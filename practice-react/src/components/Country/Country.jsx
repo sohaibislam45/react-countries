61314c8d0fcba4e905bd8b8af17ac67420a0ead1
Sohaibislam45 <sohaibislam45@gmail.com>
2025-10-08 01:31:24 +0600

    added visited btn and conditional rendering

--- practice-react/src/components/Country/Country.jsx
+++ practice-react/src/components/Country/Country.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
 
-    console.log(country)
+  console.log(country)
+  const getCountryLabel = (area, population) => {
+  if (area < 10000 && population < 1000000) return "Tiny country & Small population";
+  if (area < 10000 && population >= 1000000) return "Tiny country & Large population";
 
+  if (area < 100000 && population < 1000000) return "Small country & Small population";
+  if (area < 100000 && population >= 1000000) return "Small country & Large population";
 
-    const handleClick=()=>{
-      console.log('Clicked');
-    }
+  if (area < 1000000 && population < 1000000) return "Medium country & Small population";
+  if (area < 1000000 && population >= 1000000) return "Medium country & Large population";
+
+  if (area >= 1000000 && population < 1000000) return "Large country & Small population";
+  return "Large country & Large population";
+};
+
+  const [visited,setVisited]=useState(false);
+
+  const handleClick = () => {
+    setVisited(!visited); 
+  }
 
   const countryName = country.name;
   const flag = country.flags.flags;
@@ -21,16 +35,18 @@ const Country = ({ country }) => {
   const currencySymbol = Object.values(currencyObj)[0]?.symbol;
 
   return (
-    <div className='country-card'>
+    <div className={`country-card ${visited && 'country-visited'}`}>
       {<img src={flag.png} alt={flag.alt} />}
       <h2>Name : {countryName.common}</h2>
       <p>Official Name : {countryName.official}</p>
       <p>Region : {region}</p>
-      <p>Population : {population.toLocaleString()}</p>
+      <p>
+        Population: {population} ({getCountryLabel(country.area.area, population)})
+      </p>
       <p>Capital : {capital.join(', ')}</p>
       <p>Language : {language.join(', ')}</p>
       <p>Currency : {currencyName} ({currencySymbol})</p>
-      <button onClick={handleClick}>Not Visited</button>
+      <button onClick={handleClick}>{visited?"Visited":"Not Visited"}</button>
     </div>
   );
 };
